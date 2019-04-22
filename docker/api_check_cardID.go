bdb2799697135b3f4b0cdb43bf5f127aa17726fa
Martin Holy <martin.holy@coin-exchange.cz>
2019-04-22 19:48:25 +0200

    fix api endpoint

--- docker/api_check_cardID.go
+++ docker/api_check_cardID.go
@@ -67,6 +67,6 @@ func RCChecker(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/validate/rc/{id}", RCChecker)
+	router.HandleFunc("/validate/cardid/{id}", RCChecker)
 	log.Fatal(http.ListenAndServe(":80", router))
 }
